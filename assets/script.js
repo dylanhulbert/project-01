d8adeabe5781566a75b0e438b88521f5ac3c4735
dylan@dylanhulbert.com <Your-Email>
2020-02-25 21:25:37 -0800

    new

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+
+
+
+// need var for the parks
+var orParks = {
+    // objects for each park
+}
+
+// depending on park selected, by index, append to weather request
+// 1.    Crater Lake (crla)
+// 2.    Ice Age Floods (iafl)
+// 3.    John Day Fossil Beds (joda)
+// 4.    Lewis and Clark (lewi)
+// 5.    Nez Perce (nepe)
+
+// select date for weather request
+
+// append date and location (park) to weather request
+
