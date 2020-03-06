0303242f4c92e1eaef338fe1398ce538f56fccc6
Lauren Doss <doss.laurene@gmail.com>
2020-03-05 19:26:34 -0800

    added rounded corners to images

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,3 @@
-// javascript
-
-// var for the parks
-// var parks = {
-//     crla : {
-//         name: "crater lake",
-//         zip: "97604",
-//     },
-//     iafl : {
-//         name: "ice age floods",
-//         zip: "99116",
-//     },
-//     joda : {
-//         name: "john day fossil beds",
-//         zip: "97848",
-//     },
-//     lewi : {
-//         name: "lewis and clark",
-//         zip: "97103",
-//     },
-//     crla : {
-//         name: "nez perce",
-//         zip: "83540",
-//     }
-// };    
-
-// depending on park selected, by index, append to weather request
-
-// select date for weather request
-
-// append date and location (park) to weather request
-
 
 
 // js function for changing background image
