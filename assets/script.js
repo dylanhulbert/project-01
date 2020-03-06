a690f5fb0ad486d133b0764a3fa56a2e54dc5852
Lauren Doss <doss.laurene@gmail.com>
2020-03-05 19:14:11 -0800

    updated h3 elements and other css

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,3 @@
-// javascript
-
-// var for the parks
-// var parks = {
-//     crla : {
-//         name: "crater lake",
-//         zip: "97604",
-//     },
-//     iafl : {
-//         name: "ice age floods",
-//         zip: "99116",
-//     },
-//     joda : {
-//         name: "john day fossil beds",
-//         zip: "97848",
-//     },
-//     lewi : {
-//         name: "lewis and clark",
-//         zip: "97103",
-//     },
-//     crla : {
-//         name: "nez perce",
-//         zip: "83540",
-//     }
-// };    
-
-// depending on park selected, by index, append to weather request
-
-// select date for weather request
-
-// append date and location (park) to weather request
-
-
 
 // js function for changing background image
 $(document).ready(function () {
@@ -49,15 +16,17 @@ $(document).ready(function () {
             .then(function (response) {
             console.log(response)
 
-                $(".fiveDayForecast").text("Five Day Forecast:")
+            $(".Hours").text("Hours"); 
+            $(".Directions").text("Directions")
+                $(".fiveDayForecast").text("Five Day Forecast")
             
                 
                 var parkImg = $("<img>").attr("src", response.data[0].images[0].url);
                 var parkImg2 = $("<img>").attr("src", response.data[0].images[1].url);
                
             
-                $(".parkDirections").text("Directions: " + response.data[0].directionsInfo);
-                $(".parkHours").text("Hours of Operation: " + response.data[0].operatingHours[0].description);
+                $(".parkDirections").text(response.data[0].directionsInfo); 
+                $(".parkHours").text(response.data[0].operatingHours[0].description);
                 $(".parkImg").html(parkImg); 
                 $(".parkImg2").html(parkImg2); 
                 
@@ -98,51 +67,9 @@ $(document).ready(function () {
 
             });
 
-        // $('.pusher').css({'background-image':'url(' + [value] + ')'}, {'background-size': 'cover'});
-        // }
 
     });
 }); 
 }); 
 
-//js function for onClick event of button
-
-// $('#buttonSelector').click(function(){
-
-// })
-
-// var APIKey = "kBIqlaM6bq5UyDKlkxwMUvMo6EAuPevne8z94XHM"; 
-
-
-// var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=" + value + "&api_key=" + APIKey; 
-// console.log(parkCode)
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-
-//     })
-//     .then(function(response) {
-//         $(".crlaDirections").text("Directions: " + response.data[0].directionsInfo); 
-// $(".crlaHours").text("Hours of Operation: " + response.data[0].operatingHours[1]); 
-
-// $(".iaflDirections").text("Directions: " + response.data[0].directionsInfo); 
-// $(".iaflHours").text(
-//     "Winter Hours: " + response.data[0].operatingHours[0], 
-//     "Summer Hours: " + response.data[0].operatingHours[1]
-//     ); 
-// $(".jodaDirections").text("Directions: " + response.data[0].directionsInfo); 
-// $(".jodaHours").text("Hours of Operation: " + response.data[0].operatingHours[0]); 
-// $(".lewiDirections").text("Directions: " + response.data[0].directionsInfo); 
-// $(".lewiHours").text(
-//     "Winter Hours: " + response.data[0].operatingHours[0], 
-//     "Summer Hours: " + response.data[0].operatingHours[1]
-//  )
-//  $(".nepeDirections").text("Directions: " + response.data[0].directionsInfo)
-//  $(".nepeHours").text("Hours of Operation: " + response.data[0].operatingHours[0].description); 
-
-//     })
-
-
-// })
 
